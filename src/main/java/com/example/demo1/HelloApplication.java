5118987c717a3b6f2c7d11e8dbc3ee86f8073bd5
kmaessen <75220622+kmaessen@users.noreply.github.com>
2023-04-04 13:40:15 +0200

    hoi

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -1,32 +1,24 @@
 package com.example.demo1;
 
 
-
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
-import com.almasb.fxgl.input.Input;
-import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
-import com.almasb.fxgl.core.math.Vec2;
-import javafx.scene.text.Text;
 
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
-
 public class HelloApplication extends GameApplication{
     private Entity player;
-    private boolean loggedIn = false;
+
 
     @Override
     protected void initSettings(GameSettings settings){
@@ -39,50 +31,29 @@ public class HelloApplication extends GameApplication{
 
     @Override
     protected void initGame(){
-        if (!loggedIn) {
-            // Show login screen
-            Entity loginText = new Entity();
-            loginText.addComponent(new Text("Please log in."));
-            loginText.setPosition(new Vec2(getWidth() / 2, getHeight() / 2));
-            getGameWorld().addEntity(loginText);
-        } else {
-            player = FXGL.entityBuilder()
-                .at(400,400)
-                .viewWithBBox( "brick.png")
-                .with(new CollidableComponent(true))
-                .type(EntityTypes.PLAYER)
-                .buildAndAttach();
+        player = FXGL.entityBuilder()
+            .at(400,400)
+            .viewWithBBox( "brick.png")
+            .with(new CollidableComponent(true))
+            .type(EntityTypes.PLAYER)
+            .buildAndAttach();
 
-            FXGL.getGameTimer().runAtInterval(() ->{
-                int randomPos1 = ThreadLocalRandom.current().nextInt(80,FXGL.getGameScene().getAppWidth() - 80);
-                int randomPos2 = ThreadLocalRandom.current().nextInt(80,FXGL.getGameScene().getAppWidth() - 80);
-                FXGL.entityBuilder()
-                        .at(randomPos1,randomPos2)
-                        .viewWithBBox(new Circle(5,Color.WHITE))
-                        .with(new CollidableComponent(true))
-                        .type(EntityTypes.STAR)
-                        .buildAndAttach();
-            }, Duration.millis(2000));
-        }
-        
+        FXGL.getGameTimer().runAtInterval(() ->{
+            int randomPos1 = ThreadLocalRandom.current().nextInt(80,FXGL.getGameScene().getAppWidth() - 80);
+            int randomPos2 = ThreadLocalRandom.current().nextInt(80,FXGL.getGameScene().getAppWidth() - 80);
+            FXGL.entityBuilder()
+                    .at(randomPos1,randomPos2)
+                    .viewWithBBox(new Circle(5,Color.WHITE))
+                    .with(new CollidableComponent(true))
+                    .type(EntityTypes.STAR)
+                    .buildAndAttach();
+        }, Duration.millis(2000));
     }
+        
+
 
     @Override
     protected void initInput(){
-
-    Input input = getInput();
-
-    // Define a user action to handle login button press
-    input.addAction(new UserAction("login") {
-        @Override
-        protected void onActionBegin() {
-            if (!loggedIn) {
-                // Set loggedIn to true and reload the game
-                loggedIn = true;
-                getGameController().restartGame();
-            }
-        }
-    }, KeyCode.ENTER); // Bind to enter key for simplicity
         FXGL.onKey(KeyCode.D, () -> {
             player.translateX(5);
         });
@@ -138,8 +109,6 @@ public class HelloApplication extends GameApplication{
     protected void initGameVars(Map<String, Object> vars){
         vars.put("kills",0);
     }
-
-
 
     public static void main(String[] args) {
         launch(args);
