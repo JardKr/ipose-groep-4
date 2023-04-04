7fa97bfc753b2d1810d4b379041bff786902f18a
JardKr <122865397+JardKr@users.noreply.github.com>
2023-04-04 13:16:40 +0200

    bab
    bob

--- src/main/java/com/example/demo1/EntityTypes.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo1;
-
-    public enum EntityTypes{
-        PLAYER,  PLATFORM, EXIT_TRIGGER,  KEY_PROMPT, EXIT_SIGN, BUTTON, STAR,
-        DOOR_TOP, DOOR_BOT, MESSAGE_PROMPT
-    }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
-module com.example.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+open module mario.main {
     requires com.almasb.fxgl.all;
-
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-    opens assets.sounds;
-    opens assets.textures;
 }
